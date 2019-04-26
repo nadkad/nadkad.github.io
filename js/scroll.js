14687716017b9225ac23ab7b86c137387e11ac1a
Nadya Kadayifci <nadkad@Nadyas-MBP.home>
2019-04-26 18:27:28 -0400

    Major changes

--- js/scroll.js
+++ js/scroll.js
@@ -1,19 +1,12 @@
 // Changes the background color of the nav using inView.js plugin
 inView.threshold(0.90);
 var $nav = $('.nav');
-inView('.section').on('enter', function() {
-  $nav.css('background-color', 'rgba(0, 0, 0, 0.9)');
-})
-
-inView('#intro').on('enter', function() {
-  console.log("entered");
-  $nav.css('background-color', '');
-})
-
 inView('#footer').on('enter', function() {
-  $nav.css('background-color', '');
+  $nav.css('opacity', 0);
 })
-
+inView('#footer').on('exit', function() {
+  $nav.css('opacity', 1);
+})
 inView('#about').on('enter', function() {
 
 })
